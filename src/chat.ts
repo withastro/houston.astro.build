44f013bab0a1adf864709e6c87d10443af70dab3
Nate Moore <nate@astro.build>
2023-01-25 06:19:55 -0600

    sort answers

--- src/chat.ts
+++ src/chat.ts
@@ -1,4 +1,5 @@
-const url = "https://round-shape-acdb.pika.workers.dev/ask";
+// const url = "https://round-shape-acdb.pika.workers.dev/ask";
+const url = "http://localhost:80/ask";
 
 export async function chat(question: string) {
     const res = await fetch(url, {
@@ -14,6 +15,14 @@ export async function chat(question: string) {
       throw new Error(message);
     }
     const data = await res.json();
+    const baseURLs = new Map();
+    for (const source of data.sources) {
+      const baseURL = source.url.split('#')[0];
+      const arr = baseURLs.get(baseURL) || [];
+      arr.push(source)
+      baseURLs.set(baseURL, arr)
+    }
+    const sources = Array.from(baseURLs.values()).sort((a, b) => b.length - a.length).flat();
 
-    return data
+    return { ...data, sources }
 }
