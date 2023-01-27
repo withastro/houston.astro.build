205c767027336d2034d2e4dd2fa54da72e5dafd6
Nate Moore <nate@astro.build>
2023-01-26 21:41:15 -0600

    fix social image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: process.env.DEPLOY_URL || 'https://houston.astro.build/'
+});
